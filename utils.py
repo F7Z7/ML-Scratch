62febfb110bbd7176594d52208e842d601e35493
farza <farzanrichu610@gmail.com>
2025-06-10 02:14:07 +0530

    added spiral datasets

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from sklearn.datasets import make_moons
+# from sklearn.datasets import make_moons
+from nnfs.datasets import spiral_data
 
 
 def load_data():
-    X,y=make_moons(n_samples=100,random_state=10 )
+    # X,y=make_moons(n_samples=100,random_state=10 )
+    X, y = spiral_data(samples=100,noise=0.1)
     return X,y
