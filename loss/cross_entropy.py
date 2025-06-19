bd38580ba9feaba37b3b5cac29a18e5bfe6fc9c7
farza <farzanrichu610@gmail.com>
2025-06-20 01:28:39 +0530

    initialised the cross_entropy.py losses both forward and backward

---
+++ loss/cross_entropy.py
@@ -0,0 +1,15 @@
+import numpy as np
+class CrossEntropyLoss:
+    def forward(self, inputs, targets):
+        samples = len(inputs)
+        clipped_inputs = np.clip(inputs, 1e-7, 1 - 1e-7) #avoiding vlaus beocming to small,prevent log(0) and ensure numerical stability
+        correct_confidences = clipped_inputs[range(samples), targets]
+        negative_log=-np.log(correct_confidences)
+        cross_entropy_loss=-np.sum(negative_log)
+        return cross_entropy_loss
+
+    def backward(self,prob_values, targets):
+        samples = len(prob_values)
+        dvalues_copy = prob_values.copy()
+        dvalues_copy[range(samples), targets] -= 1
+        return dvalues_copy / samples #output/samples
