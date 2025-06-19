fe0300b79a4d4c9f5db1607cc82150ddda05a464
farza <farzanrichu610@gmail.com>
2025-06-20 01:57:52 +0530

    started combing blocks

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-# import numpy
-# import matplotlib
-# import nnfs.datasets
-print("checks done")
+from layers.activation_relu import Activation_Relu
+from layers.activation_softmax import Softmax_activation
+from layers.dense import Layer_Dense
+from loss.cross_entropy import CrossEntropyLoss
+
+
+input_layer=Layer_Dense()
+middle_layer=Layer_Dense()
+middle_layer2=Layer_Dense()
+output_layer=Layer_Dense()
