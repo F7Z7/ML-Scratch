a65393f1858ab5772e08789c3b3222450509b0a2
farza <farzanrichu610@gmail.com>
2025-06-22 03:40:05 +0530

    load iris data set

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from layers.activation_relu import Activation_Relu
 from layers.activation_softmax import Softmax_activation
 from layers.dense import Layer_Dense
 from loss.cross_entropy import CrossEntropyLoss
+from sklearn import datasets
 
 
-input_layer=Layer_Dense()
-middle_layer=Layer_Dense()
-middle_layer2=Layer_Dense()
-output_layer=Layer_Dense()
+iris = datasets.load_iris()
+print(iris.feature_names)  # 4inputs and 3 outpua
+for elem in dir(iris):
+    print(iris.elem)
+input_layer=Layer_Dense(4,10)
+middle_layer=Layer_Dense(10,10)
+middle_layer2=Layer_Dense(10,10)
+output_layer=Layer_Dense(10,3)
