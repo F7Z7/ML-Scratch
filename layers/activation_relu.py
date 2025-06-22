5de3a9872691a0892b3b1399377a88d1921d9601
farza <farzanrichu610@gmail.com>
2025-06-23 00:59:26 +0530

    minor name chnage

--- layers/activation_relu.py
+++ layers/activation_relu.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 class Activation_Relu:
-    def forward_relu(self,inputs):
+    def forward(self,inputs):
         self.inputs = inputs
         self.outputs = np.maximum(0,inputs) #relu
         return self.outputs
-    def backward_relu(self,dvalues):
+    def backward(self,dvalues):
         dinputs = dvalues.copy()
         dinputs[dvalues < 0] = 0 #if less than 0=0,else the value passes
         return dinputs
--- layers/activation_softmax.py
+++ layers/activation_softmax.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Softmax_activation:
 
-    def softmax_forward(self, inputs):
+    def forward(self, inputs):
         self.inputs = inputs
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True) #returns the value between 0 and 1
