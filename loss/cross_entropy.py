85b648f007c6b934ccabc566bab77aa836dbe966
farza <farzanrichu610@gmail.com>
2025-06-25 01:41:55 +0530

    converted to use wiht one hot encoding

--- loss/cross_entropy.py
+++ loss/cross_entropy.py
@@ -3,6 +3,10 @@ class CrossEntropyLoss:
     def forward(self, inputs, targets):
         samples = len(inputs)
         clipped_inputs = np.clip(inputs, 1e-7, 1 - 1e-7) #avoiding vlaus beocming to small,prevent log(0) and ensure numerical stability
+
+        if len(targets.shape) == 2:
+            targets = np.argmax(targets, axis=1)
+
         correct_confidences = clipped_inputs[range(samples), targets]
         negative_log=-np.log(correct_confidences)
         cross_entropy_loss=np.mean(negative_log)
@@ -10,6 +14,11 @@ class CrossEntropyLoss:
 
     def backward(self,prob_values, targets):
         samples = len(prob_values)
-        dvalues_copy = prob_values.copy()
-        dvalues_copy[range(samples), targets] -= 1
-        return dvalues_copy / samples #output/samples
+        labels=targets
+
+        if len(labels.shape) == 2:
+            labels = np.argmax(labels, axis=1)
+
+        dvalues = prob_values.copy()
+        dvalues[range(samples), labels] -= 1
+        return dvalues / samples
