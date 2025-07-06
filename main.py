08312207aaf83b7212e41e87ffbd7bcf29bcc0e3
farza <farzanrichu610@gmail.com>
2025-07-07 02:07:24 +0530

    reformted the code to do the updation of weight in new way

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from activations.activation_softmax import Softmax_activation
 from layers.dense import Layer_Dense
 from loss.cross_entropy import CrossEntropyLoss
 from sklearn import datasets
-from sklearn.model_selection import train_test_split #for splitting the data to test and train
+from sklearn.model_selection import train_test_split  # for splitting the data to test and train
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import classification_report, confusion_matrix
-
 
 import matplotlib.pyplot as plt
 
 Wine = load_wine()
 
-X=Wine.data
-y=Wine.target.reshape(-1,1) #reshaping for one hot encoding
+X = Wine.data
+y = Wine.target.reshape(-1, 1)  # reshaping for one hot encoding
 print(y.shape)
 # print(x,y)
 # print(iris.feature_names)  # 4inputs and 3 outpua
 
-#plot scattering
+# plot scattering
 # _,ax=plt.subplots()
 # scatter = ax.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
 # ax.set(xlabel=iris.feature_names[0], ylabel=iris.feature_names[1])
@@ -32,47 +31,45 @@ print(y.shape)
 encoder = OneHotEncoder(sparse_output=False)
 y_encoded = encoder.fit_transform(y)
 # print(y_encoded)
-#splitin datasets into 80:20 format
+# splitin datasets into 80:20 format
 # y=iris.target.reshape(-1,1)
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=25)
 
-#3 layer network
-input_layer=Layer_Dense(13,100)
-middle_layer=Layer_Dense(100,100)
-output_layer=Layer_Dense(100,3)
+# 3 layer network
+input_layer = Layer_Dense(13, 100)
+middle_layer = Layer_Dense(100, 100)
+output_layer = Layer_Dense(100, 3)
 
-#activations
-relu=Activation_Relu()
-softmax=Softmax_activation()
-#losses
-loss=CrossEntropyLoss()
-losses=[]
-accuracies=[]
-epoch=1000 #no of iterations
-lr=0.05      #learning rate
-
+# activations
+relu = Activation_Relu()
+softmax = Softmax_activation()
+# losses
+loss = CrossEntropyLoss()
+losses = []
+accuracies = []
+epoch = 1000  # no of iterations
+lr = .1  # learning rate
 
 for epoch in range(epoch):
-    #input layer
-    first_layer_input=input_layer.forward(X_train)
-    first_relu_output=relu.forward(first_layer_input) #relu out of input
+    # input layer
+    first_layer_input = input_layer.forward(X_train)
+    first_relu_output = relu.forward(first_layer_input)  # relu out of input
 
-    #middle layer
-    second_layer_input=middle_layer.forward(first_relu_output)
-    second_relu_output=relu.forward(second_layer_input)
+    # middle layer
+    second_layer_input = middle_layer.forward(first_relu_output)
+    second_relu_output = relu.forward(second_layer_input)
 
-    #output lauyer->softmax instead of relu
-    final_layer_input=output_layer.forward(second_relu_output)
-    final_softmax_output=softmax.forward(final_layer_input)
+    # output lauyer->softmax instead of relu
+    final_layer_input = output_layer.forward(second_relu_output)
+    final_softmax_output = softmax.forward(final_layer_input)
 
+    # losss
+    output_loss = loss.forward(final_softmax_output, y_train)
 
-    #losss
-    output_loss=loss.forward(final_softmax_output, y_train)
-
-    #accuracy
-    prediction=np.argmax(final_softmax_output,axis=1)
+    # accuracy
+    prediction = np.argmax(final_softmax_output, axis=1)
     # true_classes=np.argmax(y_train,axis=1) #correct
-    accuracy=np.mean(np.argmax(final_softmax_output, axis=1) == np.argmax(y_train, axis=1))
+    accuracy = np.mean(np.argmax(final_softmax_output, axis=1) == np.argmax(y_train, axis=1))
 
     losses.append(output_loss)
     accuracies.append(accuracy)
@@ -80,19 +77,20 @@ for epoch in range(epoch):
     if epoch % 100 == 0:
         print(f"Epoch {epoch}, Loss: {output_loss:.4f}, Accuracy: {accuracy * 100:.2f}%")
 
+    # backpropoagtion
+    dloss = loss.backward(final_softmax_output, y_train)
+    back_input = output_layer.backward(dloss)
 
+    drelu_middle = relu.backward(back_input)
+    dmiddle = middle_layer.backward(drelu_middle)
 
-    #backpropoagtion
-    dloss=loss.backward(final_softmax_output,y_train)
-    back_input=output_layer.backward(dloss,lr)
+    drelu_input = relu.backward(dmiddle)
+    dinput = input_layer.backward(drelu_input)
 
-    drelu_middle=relu.backward(back_input)
-    dmiddle=middle_layer.backward(drelu_middle,lr)
+    for layers in [output_layer, middle_layer, input_layer]:
+        layers.update_params(lr)
 
-    drelu_input=relu.backward(dmiddle)
-    dinput=input_layer.backward(drelu_input,lr)
-
-final_avg_accuray=accuracy.mean()
+final_avg_accuray = accuracy.mean()
 print(f"Final average accuracy is {final_avg_accuray * 100:.2f}")
 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
@@ -114,24 +112,24 @@ axs[1].grid(True)
 
 plt.tight_layout()
 plt.show()
-#testing of datasets
+# testing of datasets
 
-test_out1=input_layer.forward(X_test)
-test_relu1=relu.forward(test_out1)
-test_out2=middle_layer.forward(test_relu1)
-test_relu2=relu.forward(test_out2)
-test_out3=output_layer.forward(test_relu2)
-test_final_output=softmax.forward(test_out3)
+test_out1 = input_layer.forward(X_test)
+test_relu1 = relu.forward(test_out1)
+test_out2 = middle_layer.forward(test_relu1)
+test_relu2 = relu.forward(test_out2)
+test_out3 = output_layer.forward(test_relu2)
+test_final_output = softmax.forward(test_out3)
 
-
-test_prediction=np.argmax(test_final_output,axis=1)
-test_true_labels=y_test.argmax(axis=1)
-test_accuracy=np.mean(test_prediction==test_true_labels)
+test_prediction = np.argmax(test_final_output, axis=1)
+test_true_labels = y_test.argmax(axis=1)
+test_accuracy = np.mean(test_prediction == test_true_labels)
 
 print(f"\n✅ Final Test Accuracy: {test_accuracy * 100:.2f}%")
 
-#confusion matrices
-Confusion_Matrix=confusion_matrix(test_true_labels, test_prediction)
+# confusion matrices
+Confusion_Matrix = confusion_matrix(test_true_labels, test_prediction)
 print(Confusion_Matrix)
 
-print(f"Classification Report{classification_report(test_true_labels, test_prediction, target_names=Wine.target_names)}")
+print(
+    f"Classification Report{classification_report(test_true_labels, test_prediction, target_names=Wine.target_names)}")
