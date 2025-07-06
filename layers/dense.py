35c2b3f4bd6a87b4e07430be88361318ea9a286c
farza <farzanrichu610@gmail.com>
2025-07-07 02:02:03 +0530

    small indenaion update

--- layers/dense.py
+++ layers/dense.py
@@ -1,23 +1,26 @@
 import numpy as np
+
+
 class Layer_Dense:
-    def __init__(self,inputs,neurons):
-        self.weights=np.random.randn(inputs,neurons)*np.sqrt(2/inputs)
-        self.biases=np.zeros((1, neurons))
+    def __init__(self, inputs, neurons):
+        self.weights = np.random.randn(inputs, neurons) * np.sqrt(2 / inputs)
+        self.biases = np.zeros((1, neurons))
 
     def forward(self, inputs):
-        self.inputs=inputs
-        self.output=np.dot(self.inputs,self.weights)+self.biases #y=mx+c
+        self.inputs = inputs
+        self.output = np.dot(self.inputs, self.weights) + self.biases  # y=mx+c
 
         return self.output
-    def backward(self,dvalues,lr):
-        self.dweights=np.dot(self.inputs.T, dvalues)
-        self.dbiases=np.sum(dvalues,axis=0) #summing all biases
 
-        self.dinputs=np.dot(dvalues, self.weights.T)
+    def backward(self, dvalues, lr):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0)  # summing all biases
 
-        #new weight=pld weight-lr*loss
-        self.weights-=lr*self.dweights
-        self.biases-=lr*self.dbiases
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+        # new weight=pld weight-lr*loss
+        self.weights -= lr * self.dweights
+        self.biases -= lr * self.dbiases
 
         return self.dinputs
 
