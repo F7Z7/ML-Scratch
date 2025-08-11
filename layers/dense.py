a1438a9d44dbde295f1eec1c5ceda2547caa6f3f
farza <farzanrichu610@gmail.com>
2025-08-11 21:05:50 +0530

    setup keepdims=true

--- layers/dense.py
+++ layers/dense.py
@@ -14,7 +14,7 @@ class Layer_Dense:
 
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
-        self.dbiases = np.sum(dvalues, axis=0)  # summing all biases
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
 
         self.dinputs = np.dot(dvalues, self.weights.T)
 
