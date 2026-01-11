31f24d88a7aa13eb60dc854c5aaf866aa497ce3c
Farzan R S <farzanrichu610@gmail.com>
2026-01-11 23:59:18 +0530

    added a show results section

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from models.wineDataset_test_model import run as run_wine
 from models.iris_test_model import run as run_iris
 from models.breastCancer_Model import run as run_bc
+from models.mnist_model import run as run_mnist
 
 if __name__ == "__main__":
-    run_wine()
-    run_iris()
-    run_bc()
+    # run_wine()
+    # run_iris()
+    # run_bc()
+    run_mnist()
